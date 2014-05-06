62eaded2482403661f52ff505793b7a24dbd5261
Rafi Jacoby <rjacoby@astrokid.com>
2014-05-06 12:39:34 -0500

    working next slide

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var applescript = require("applescript");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PowerPoint Server' });
+});
+
+/* GET next slide. */
+router.get('/next', function(req, res) {
+  var script = "tell application \"Microsoft PowerPoint\" to go to next slide (slide show view of slide show window 1)";
+
+  applescript.execString(script, function(err, rtn) {
+    if (err) {
+      // Something went wrong!
+      console.log(err);
+    }
+  });
+  res.render('index', { title: 'PowerPoint Server NEXT SLIDE' });
 });
 
 module.exports = router;
